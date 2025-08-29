d5976d9aa6ab9f53bde52c068d2858d5cd58be7d
CosLynx AI <drixisgod@gmail.com>
2025-08-29 23:13:06 +0530

    generated file: `src/slices/index.ts` [slices]

---
+++ src/slices/index.ts
@@ -0,0 +1,11 @@
+import * as Contact from './Contact';
+import * as ScrollExperience from './ScrollExperience';
+import * as Carousel from './Carousel';
+import * as Hero from './Hero';
+
+export {
+  Contact,
+  ScrollExperience,
+  Carousel,
+  Hero
+};
